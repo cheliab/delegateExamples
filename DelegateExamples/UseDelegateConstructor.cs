512113d07004bade904ad54731fd47b891c209b5
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2020-08-19 00:00:21 +1000

    Пример добавления метода в делегат через конструктор

---
+++ DelegateExamples/UseDelegateConstructor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DelegateExamples
+{
+    /// <summary>
+    /// Присвоение ссылки на метод используя конструктор делегата
+    /// </summary>
+    public class UseDelegateConstructor
+    {
+        delegate int Operation(int x, int y);
+
+        public static void Run()
+        {
+            Operation operationAdd = new Operation(Add);
+            int result = operationAdd(3, 7);
+            
+            Console.WriteLine(result);
+        }
+
+        private static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
