9ab1abdcc55daee1b9e7d65461b6fa543f4b6c13
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2020-08-19 00:43:50 +1000

    Пример добавления нескольких методов в делегат

---
+++ DelegateExamples/MultipleMethodsInDelegate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DelegateExamples
+{
+    public class MultipleMethodsInDelegate
+    {
+        delegate void Message();
+
+        public static void Run()
+        {
+            Message messageDelegate;
+
+            messageDelegate = Hello;
+            messageDelegate += HowAreYou;
+
+            messageDelegate();
+        }
+
+        private static void Hello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        private static void HowAreYou()
+        {
+            Console.WriteLine("How are you?");
+        }
+    }
+}
--- DelegateExamples/Program.cs
+++ DelegateExamples/Program.cs
@@ -6,11 +6,17 @@ namespace DelegateExamples
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(new string('-', 40));
+            
             SimpleDelegate.Run();
             
             Console.WriteLine(new string('-', 40));
             
             OperationDelegate.Run();
+            
+            Console.WriteLine(new string('-', 40));
+            
+            MultipleMethodsInDelegate.Run();
             
             Console.WriteLine(new string('-', 40));
             
